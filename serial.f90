5b428d989b2a8e35754fbb847f1464939e959e11
James Riley Wynne III <wynnejr@ornl.gov>
2014-03-03 16:27:03 -0500

    First attempt of a fortran monte carlo operation. linker gives error about utime and init_random_seed

---
+++ serial.f90
@@ -0,0 +1,22 @@
+        program serialpi
+                implicit none
+                integer :: i, time
+                real :: niter, x, y, inCircle, z, pi
+                i=0
+                inCircle = 0
+                niter = 100000
+                
+                call utime(time)
+                call init_random_seed(time)
+
+                do i=0,niter
+                        call random_number(x)
+                        call random_number(y)
+                        z = sqrt(x**2+y**2)
+                        if (z <= 1) then
+                                inCircle = inCircle + 1
+                        end if
+                enddo
+                
+                pi = 4*(inCircle/niter)
+        end program
